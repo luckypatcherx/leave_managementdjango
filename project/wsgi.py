219b39c2a934bac7586cda5c9c60c9635446a77c
luckypatcherx <abhiramtn54@gmail.com>
2022-12-31 09:03:49 +0530

    updated wsgi file
    added settings for hosting the fil.e.

--- project/wsgi.py
+++ project/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
+#add your project directory
 
-from django.core.wsgi import get_wsgi_application
+project_home= '/home/abhiramtn/leave_managementdjango'
+if project_home not in sys.path:
+    sys.path.insert(0,project_home)
 
+
+
+
+#set environment variable to tell django where your serttings.py file is
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
-
+# serve django via WSGI
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
